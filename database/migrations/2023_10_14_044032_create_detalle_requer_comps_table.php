5cfb6190285cb8cc87ea3819525346be85e5d0a9
Pablo-935 <pabloferreyra935@gmail.com>
2023-10-23 01:30:00 -0300

    Base del Proyecto v2

--- database/migrations/2023_10_14_044032_create_detalle_requer_comps_table.php
+++ database/migrations/2023_10_14_044032_create_detalle_requer_comps_table.php
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('detalle_requer_comps');
+        Schema::dropIfExists('detalle_requer_compras');
     }
 };
